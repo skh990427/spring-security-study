8fecb8308a014f375d1dff09fff41f89e9cd1fa8
송광호 <skh990427@naver.com>
2025-03-26 22:18:36 +0900

    feat :: 사용자 정의 보안 설정하기

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    /**
+     * 스프링 시큐리티 사용자 추가 방법2 - 자바 파일에 빈으로 등록
+     * 프로퍼티랑 같이 있으면 얘가 우선순위를 가진다
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .authorities("USER")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}1111")
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, user2);
+    }
+}
