cf3b8113dfcefbfbb975414eff5691d1e7a649b7
송광호 <skh990427@naver.com>
2025-04-13 02:18:35 +0900

    섹션3 :: AuthenticationManager - 커스텀 필터 + 프로바이더(부모)

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,57 +1,57 @@
-package com.example.security.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@EnableWebSecurity
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        AuthenticationManager authenticationManager = builder.build();
-//        AuthenticationManager authenticationManager1 = builder.getObject(); //다른곳에서 가져올때는 getObject() 사용해야함
-
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/api/login").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .authenticationManager(authenticationManager)
-                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
-        ;
-        return http.build();
-    }
-
-    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
-        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
-        return customAuthenticationFilter;
-    }
-
-    /**
-     * 스프링 시큐리티 사용자 추가 방법2 - 자바 파일에 빈으로 등록
-     * 프로퍼티랑 같이 있으면 얘가 우선순위를 가진다
-     */
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}1111")
-                .authorities("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-}
+//package com.example.security.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+//        AuthenticationManager authenticationManager = builder.build();
+////        AuthenticationManager authenticationManager1 = builder.getObject(); //다른곳에서 가져올때는 getObject() 사용해야함
+//
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/", "/api/login").permitAll()
+//                        .anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults())
+//                .authenticationManager(authenticationManager)
+//                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
+//        ;
+//        return http.build();
+//    }
+//
+//    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+//        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+//        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+//        return customAuthenticationFilter;
+//    }
+//
+//    /**
+//     * 스프링 시큐리티 사용자 추가 방법2 - 자바 파일에 빈으로 등록
+//     * 프로퍼티랑 같이 있으면 얘가 우선순위를 가진다
+//     */
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.withUsername("user")
+//                .password("{noop}1111")
+//                .authorities("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+//}
