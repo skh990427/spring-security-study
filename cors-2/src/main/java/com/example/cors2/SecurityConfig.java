d68b351acdeef9d9a7964686be62dacb9e48205d
송광호 <skh990427@naver.com>
2025-04-26 19:03:11 +0900

    섹션8 :: cors(예비요청 포함)

---
+++ cors-2/src/main/java/com/example/cors2/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.cors2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        ;
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true); // 쿠키나 인증등의 보안관련 요소들 처리 할 수 있도록 true
+        configuration.setMaxAge(3600L); // 사전 요청일 경우 계속 요청하는 것이 아니라 캐시 타임 안에서는 가져온 요청을 바로 쓸 수 있도록 설정
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    /**
+     * 스프링 시큐리티 사용자 추가 방법2 - 자바 파일에 빈으로 등록
+     * 프로퍼티랑 같이 있으면 얘가 우선순위를 가진다
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
