f5ac29c4ad616daee3787b0f8bffdee56923eba4
송광호 <skh990427@naver.com>
2025-05-11 15:47:04 +0900

    섹션9 인가프로세스 :: authorizeHttpRequests

--- src/main/java/com/example/security/IndexController.java
+++ src/main/java/com/example/security/IndexController.java
@@ -5,32 +5,49 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public Authentication index(Authentication authentication) {
-        return authentication;
+    @GetMapping("/user")
+    public String user(){
+        return "user";
     }
 
-    @GetMapping("/anonymous")
-    public String anonymous() {
-        return "anonymous";
+    @GetMapping("/myPage/points")
+    public String myPage(){
+        return "myPage";
     }
 
-    @GetMapping("/anonymousContext")
-    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
-        return context.getAuthentication().getName();
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
     }
 
-    @GetMapping("/authentication")
-    public String authentication(Authentication authentication) {
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return "anonymous";
-        } else {
-            return "not anonymous";
-        }
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/admin/payment")
+    public String adminPayment(){
+        return "adminPayment";
+    }
+
+    @GetMapping("/resource/address_01")
+    public String address_01(){
+        return "address_01";
+    }
+
+    @GetMapping("/resource/address01")
+    public String address01(){
+        return "address01";
+    }
+
+    @PostMapping("/post")
+    public String post(){
+        return "post";
     }
 }
