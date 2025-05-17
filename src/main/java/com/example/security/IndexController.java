f0b7a2f0c84bcb9ea2f8bd0cdd5414feb4f8f38e
송광호 <skh990427@naver.com>
2025-05-17 13:38:39 +0900

    섹션9 인가프로세스 :: 정적 자원 관리

--- src/main/java/com/example/security/IndexController.java
+++ src/main/java/com/example/security/IndexController.java
@@ -1,36 +1,14 @@
 package com.example.security;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public Authentication index(Authentication authentication) {
-        return authentication;
+    @GetMapping("/user")
+    public String user(){
+        return "user";
     }
 
-    @GetMapping("/anonymous")
-    public String anonymous() {
-        return "anonymous";
-    }
-
-    @GetMapping("/anonymousContext")
-    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
-        return context.getAuthentication().getName();
-    }
-
-    @GetMapping("/authentication")
-    public String authentication(Authentication authentication) {
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return "anonymous";
-        } else {
-            return "not anonymous";
-        }
-    }
 }
--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,12 +19,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+//    @Bean // 이젠 이 방식보단 시큐리티 6에서는 성능문제 해결돼서 .permitAll() 쓰는게 좋음
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()); //StaticResourceLocation.class 의 경로들 검사 무시
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/image/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
     }
