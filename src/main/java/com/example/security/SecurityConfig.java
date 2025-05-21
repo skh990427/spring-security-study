dc69542d442b1f877705617984e33645f39a5628
송광호 <skh990427@naver.com>
2025-05-21 23:44:31 +0900

    섹션10 인가 아키텍처 :: RequestMatcherDelegatingAuthorizationManager 인가 설정 응용

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.example.security;
 
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +17,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEntry;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+@Slf4j
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
@@ -19,17 +31,44 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().access(authorizationManager(null)))
+            .formLogin(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
     }
 
+    @Bean
+    public AuthorizationManager<RequestAuthorizationContext> authorizationManager(
+        HandlerMappingIntrospector introspector) {
+        List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings = new ArrayList<>();
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry1
+            = new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/user"),
+                                        AuthorityAuthorizationManager.hasAuthority("ROLE_USER"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry2
+            = new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/db"),
+                                        AuthorityAuthorizationManager.hasAuthority("ROLE_DB"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry3
+            = new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/admin"),
+                                        AuthorityAuthorizationManager.hasAuthority("ROLE_admin"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry4
+            = new RequestMatcherEntry<>(AnyRequestMatcher.INSTANCE, new AuthenticatedAuthorizationManager<>());
+
+        mappings.add(requestMatcherEntry1);
+        mappings.add(requestMatcherEntry2);
+        mappings.add(requestMatcherEntry3);
+        mappings.add(requestMatcherEntry4);
+
+        return new CustomRequestMatcherDelegatingAuthorizationManager(mappings);
+    }
+
     /**
-     * 스프링 시큐리티 사용자 추가 방법2 - 자바 파일에 빈으로 등록
-     * 프로퍼티랑 같이 있으면 얘가 우선순위를 가진다
+     * 스프링 시큐리티 사용자 추가 방법2 - 자바 파일에 빈으로 등록 프로퍼티랑 같이 있으면 얘가 우선순위를 가진다
      */
     @Bean
     public UserDetailsService userDetailsService() {
