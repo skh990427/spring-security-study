4dd842315bbcbe98a6077ec5aa6562231a6bffd9
송광호 <skh990427@naver.com>
2025-05-23 00:05:17 +0900

    섹션10 인가 아키텍처 :: 포인트컷 보안메서드 구현하기

--- src/main/java/com/example/security/DataService.java
+++ src/main/java/com/example/security/DataService.java
@@ -1,18 +1,14 @@
 package com.example.security;
 
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataService {
 
-    @PreAuthorize(value = "")
     public String getUser() {
         return "user";
     }
 
-    @PostAuthorize(value = "")
     public Account getOwner(String name) {
         return new Account(name, false);
     }
--- src/main/java/com/example/security/IndexController.java
+++ src/main/java/com/example/security/IndexController.java
@@ -6,22 +6,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    private final DataService dataService;
+
+    public IndexController(DataService dataService) {
+        this.dataService = dataService;
+    }
+
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
+
     @GetMapping("/user")
-    public String user(){
-        return "user";
+    public String user() {
+        return dataService.getUser();
     }
 
-    @GetMapping("/db")
-    public String db(){
-        return "db";
+    @GetMapping("/owner")
+    public Account owner(String name) {
+        return dataService.getOwner(name);
     }
 
-    @GetMapping("/admin")
-    public String admin(){
-        return "admin";
+    @GetMapping("/display")
+    public String display() {
+        return dataService.display();
     }
 }
