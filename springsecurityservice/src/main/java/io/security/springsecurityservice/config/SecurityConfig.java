f042b15ca93b74ae0559963e7fec4b4a401db9e9
송광호 <skh990427@naver.com>
2025-06-02 00:25:06 +0900

    회원 인증 시스템 :: 로그인 페이지 만들기

--- springsecurityservice/src/main/java/io/security/springsecurityservice/config/SecurityConfig.java
+++ springsecurityservice/src/main/java/io/security/springsecurityservice/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package io.security.springsecurityservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,9 +17,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
                 .requestMatchers("/").permitAll()
                 .anyRequest().authenticated())
-            .formLogin(Customizer.withDefaults())
+            .formLogin(form -> form.loginPage("/login").permitAll())
         ;
         return http.build();
     }
