f9464f461b31332d07246202e54f24d76484b0a2
송광호 <skh990427@naver.com>
2025-06-01 23:37:46 +0900

    회원 인증 시스템 :: 프로젝트 생성 및 기본 구성

---
+++ springsecurityservice/src/test/java/io/security/springsecurityservice/SpringsecurityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.springsecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
