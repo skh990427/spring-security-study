b0478c775cb43c32d7a59be9958171fa7d671771
송광호 <skh990427@naver.com>
2025-06-06 20:00:44 +0900

    비동기 인증 :: Rest 인증 보안 및 화면 구성

--- springsecurityservice/src/main/java/io/security/springsecurityservice/security/config/SecurityConfig.java
+++ springsecurityservice/src/main/java/io/security/springsecurityservice/security/config/SecurityConfig.java
@@ -5,10 +5,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -43,6 +45,19 @@ public class SecurityConfig {
             .authenticationProvider(authenticationProvider)
             .exceptionHandling(exception -> exception
                                    .accessDeniedHandler(new FormAccessDeniedHandler("/denied")))
+        ;
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .securityMatcher("/api/**")
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
+                .anyRequest().permitAll())
+            .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
     }
